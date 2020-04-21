4db542907c1d000ccf6a052b2fa2134310728f08
Hung Tran <hung123hung456@gmail.com>
2020-04-22 01:33:02 +0700

    update server-app

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # S.E.P
 Music App
+
+-----
+> **USEAGE**
+
+### Build:
+
+  ```
+  $ source .env
+  ```
+
+  - Using <a href="https://www.docker.com/">Docker</a>  
+
+    ```
+    $ docker-compose up
+    ```
+  - Open Terminal
+    ```
+    $ docker exec -it mongodb bash (To open execution path of mongodb in docker image )
+    $ mongo -u admin -p admin --authenticationDatabase musicapp 
+    ```
+
+  - Run Server:
+    ```
+    $ localhost:5000
+    ```
+
+### API references:
+ 
+### Document:
+ - <a href="https://flask-pymongo.readthedocs.io/en/latest/">Flask-PyMongo</a> *Server Useage*
+
+ - <a href="https://www.digitalocean.com/community/tutorials/how-to-set-up-flask-with-mongodb-and-docker">flask-mongodb docker</a> *How to build docker for flask and mongodb*
+
+ - <a href="https://nosqlbooster.com/downloads">NosqlBooster</a> *GUI Tool for MongoDB*
+
+
+ ### Task Lists:
+
+    
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,26 +1,26 @@
 import os
-from flask import Flask, request, jsonify
+from flask import  request, jsonify,Flask
 from flask.logging import default_handler
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.logger.removeHandler(default_handler)
-app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGO_ADMIN_USER'] + ':' + os.environ['MONGO_ADMIN_PWD'] + ':27000/' + os.environ['MONGO_DATABASE']
+app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGO_ADMIN_USER'] + ':' + os.environ['MONGO_ADMIN_PWD'] + '@' + os.environ['MONGO_HOSTNAME'] + ':27017/' + os.environ['MONGO_DATABASE']
 
 mongo = PyMongo(app)
 
 db =mongo.db
 
-# @app.route('/')
-# def index():
-#     return jsonify(
-#         status = True,
-#         message = 'Successs connect to MongoDB!'
-#     )
 @app.route('/')
 def index():
-    # user_collection = mongo.db.users
-    # user_collection.insert({'name' : 'Cristina'})
-    # user_collection.insert({'name' : 'Derek'})
-    return '<h1>Added a User!</h1>'
+    return jsonify(
+        status = True,
+        message = 'Successs connect to MongoDB!'
+    )
+# @app.route('/')
+# def index():
+#     # user_collection = mongo.db.users
+#     # user_collection.insert({'name' : 'Cristina'})
+#     # user_collection.insert({'name' : 'Derek'})
+#     return '<h1>Added a User!</h1>'
 
